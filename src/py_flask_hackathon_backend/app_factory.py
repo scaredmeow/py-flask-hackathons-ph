1fc3318f7b5543fe25bfaf8cb023f2c3ed1c46e8
Neil Riego <neilchristianriego3@gmail.com>
2024-04-10 01:47:20 +0800

    db[init]: Add migrations files for Flask-SQLAlchemy and Alembic

--- src/py_flask_hackathon_backend/app_factory.py
+++ src/py_flask_hackathon_backend/app_factory.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 from src.config import Config
-from src.deps import fairy, blueprint, ma
+from src.deps import fairy, blueprint, ma, db
+from utils import import_name
 
 
 def create_app(config=Config) -> Flask:
@@ -8,10 +10,17 @@ def create_app(config=Config) -> Flask:
 
     app.config.from_object(config)
 
-    # db.init_app(app)
+    db.init_app(app)
     ma.init_app(app)
     fairy.init_app(app)
+
+    # Register blueprints
     blueprint.register_blueprints(app)
+
+    # Register models
+    for model in os.listdir("src/models"):
+        if model != "__init__.py" and model.endswith(".py"):
+            import_name("src.models", model.replace(".py", ""))
 
     @app.route("/")
     def hello_world():

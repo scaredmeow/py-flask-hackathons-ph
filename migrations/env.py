929c47e37eb1f240423d672e96f136d225c5f348
Neil Riego <neilchristianriego3@gmail.com>
2024-04-10 01:54:07 +0800

    Refactor code to use double quotes instead of single quotes for consistency

--- migrations/env.py
+++ migrations/env.py
@@ -12,31 +12,30 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
-logger = logging.getLogger('alembic.env')
+logger = logging.getLogger("alembic.env")
 
 
 def get_engine():
     try:
         # this works with Flask-SQLAlchemy<3 and Alchemical
-        return current_app.extensions['migrate'].db.get_engine()
+        return current_app.extensions["migrate"].db.get_engine()
     except (TypeError, AttributeError):
         # this works with Flask-SQLAlchemy>=3
-        return current_app.extensions['migrate'].db.engine
+        return current_app.extensions["migrate"].db.engine
 
 
 def get_engine_url():
     try:
-        return get_engine().url.render_as_string(hide_password=False).replace(
-            '%', '%%')
+        return get_engine().url.render_as_string(hide_password=False).replace("%", "%%")
     except AttributeError:
-        return str(get_engine().url).replace('%', '%%')
+        return str(get_engine().url).replace("%", "%%")
 
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from src.deps.db import metadata # noqa
+from src.deps.db import metadata  # noqa
 
 target_metadata = metadata
 config.set_main_option("sqlalchemy.url", get_engine_url())
@@ -49,7 +48,7 @@ target_db = current_app.extensions["migrate"].db
 
 
 def get_metadata():
-    if hasattr(target_db, 'metadatas'):
+    if hasattr(target_db, "metadatas"):
         return target_db.metadatas[None]
     return target_db.metadata
 
@@ -67,9 +66,7 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url, target_metadata=get_metadata(), literal_binds=True
-    )
+    context.configure(url=url, target_metadata=get_metadata(), literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -112,7 +109,10 @@ def run_migrations_online():
         #     connection=connection, target_metadata=get_metadata(), **conf_args
         # ) # DISABLES GUARD FOR DROP AND ALTERING TABLES
         context.configure(
-            connection=connection, target_metadata=get_metadata(), include_object=include_object, **conf_args
+            connection=connection,
+            target_metadata=get_metadata(),
+            include_object=include_object,
+            **conf_args,
         )
 
         with context.begin_transaction():

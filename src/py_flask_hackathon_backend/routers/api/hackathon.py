2e5d39b0c2b9a619db4620298e6b3445720e27bb
Neil Riego <neilchristianriego3@gmail.com>
2024-04-10 20:27:42 +0800

    feat[API]: Add API endpoints

--- src/py_flask_hackathon_backend/routers/api/hackathon.py
+++ src/py_flask_hackathon_backend/routers/api/hackathon.py
@@ -1,12 +1,72 @@
-from apifairy import response
-from flask import Blueprint
+from apifairy import response, body
+from flask import Blueprint, abort
 
-from src.schemas import HTTPRequestSchema
+from models.hackathons import Hackathon
+from src.schemas import HackathonSchema, OKRequestSchema, UploadDocumentSchema
+from src.decorators import paginated_response
 
 app = Blueprint("app", __name__)
 
 
 @app.route("/")
-@response(HTTPRequestSchema)
-def index():
-    return {"code": 200, "message": "Success", "description": "Hello, World!"}
+@paginated_response(schema=HackathonSchema(exclude=["upload"]))
+def hackathons(data: dict):
+    """Get all hackathons"""
+    print(data)
+    return Hackathon.query
+
+
+@app.route("/<int:id>")
+@response(HackathonSchema)
+def hackathon(id):
+    """Get hackathon by id"""
+    return Hackathon.query.get_or_404(id)
+
+
+@app.route("/", methods=["POST"])
+@body(HackathonSchema(exclude=["id", 'upload']))
+@response(OKRequestSchema)
+def create_hackathon(data: dict):
+    """Create a new hackathon"""
+    try:
+        hackathon = Hackathon(**data)
+        hackathon.add_record()
+    except Exception as e:
+        abort(400, str(e))
+
+
+@app.route("/<int:id>", methods=["PUT"])
+@body(HackathonSchema(exclude=["id", 'upload']))
+@response(OKRequestSchema)
+def update_hackathon(data: dict, id: int):
+    """Update hackathon by id"""
+    try:
+        hackathon = Hackathon.query.get_or_404(id)
+        hackathon.update_record(**data)
+    except Exception as e:
+        abort(400, str(e))
+
+
+@app.route("/<int:id>", methods=["DELETE"])
+@response(OKRequestSchema)
+def delete_hackathon(id: int):
+    """Delete hackathon by id"""
+    try:
+        hackathon = Hackathon.query.get_or_404(id)
+        hackathon.delete_record()
+    except Exception as e:
+        abort(400, str(e))
+
+
+@app.route("/<int:id>/image", methods=["POST"])
+@body(UploadDocumentSchema, location="form", media_type="multipart/form-data")
+@response(OKRequestSchema)
+def upload_image(data: dict, id: int):
+    """Upload image for hackathon"""
+    document = data.get("document")
+
+    try:
+        hackathon = Hackathon.query.get_or_404(id)
+        hackathon.insert_image(document)
+    except Exception as e:
+        abort(400, str(e))
